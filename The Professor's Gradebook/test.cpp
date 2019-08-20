2d7a02b9ec29388a86f265d04a2e43c6251ade2f
MorningMoment <jane.c.xing@gmail.com>
2019-08-20 09:24:59 +0800

    prof's gradebook, adjust max'

--- The Professor's Gradebook/test.cpp
+++ The Professor's Gradebook/test.cpp
@@ -16,61 +16,57 @@ using namespace std;
 
 
 void show_array(double *array,int size){
-	for(int i=0;i<size;i++){
-		cout<<array[i]<<' ';
-	}
-	cout<<endl;
+    for(int i=0;i<size;i++){
+        cout<<array[i]<<' ';
+    }
+    cout<<endl;
 }
 
-void show_array(char *array[],int size){
-	for(int i=0;i<size;i++){
-		cout<<array[i]<<' ';
-	}
-	cout<<endl;
-}
 
 char letter(int demo){
-	if(demo<=100 && demo>=90) return 'A';
-	else if(demo<90 && demo>=80) return 'B';
-	else if(demo<80 && demo>=70) return 'C';
-	else if(demo<70 && demo>=60) return 'D';
-	else return 'F';
+    if(demo<=100 && demo>=90) return 'A';
+    else if(demo<90 && demo>=80) return 'B';
+    else if(demo<80 && demo>=70) return 'C';
+    else if(demo<70 && demo>=60) return 'D';
+    else return 'F';
 }
 
 int main(){
-	int stu_num,ass_num;
-	cin>>stu_num>>ass_num;
-	char *name[max_stu];
-	double total[max_stu];
-	double adjust[max_stu];
-	
-	for(int i=0;i<stu_num;i++){
-		char *p = new char[3];
-		double min_score = 1000, sum = 0;
-		cin>>p;
-		name[i] = p;
-		for(int j=0;j<ass_num;j++){
-			double demo;
-			cin>>demo;
-			min_score = min_score<demo?min_score:demo;
-			sum+=demo;
-		} 
-		double exam;
-		cin>>exam;
-		sum += exam;
-		sum -= min_score;
-		total[i] = sum;
-	
-	}
-	
-	//adjust scores
-	double max_score = *max_element(total,total+max_stu);
-	for(int i=0;i<stu_num;i++){
-		adjust[i] = ceil(total[i]*100/max_score);
-		cout<<name[i]<<' '<<total[i]<<' '<<adjust[i]<<' '<<letter(adjust[i])<<endl;
-	}
-	
-	
-	
-	return 0;
+    int stu_num,ass_num;
+    cin>>stu_num>>ass_num;
+    char *name[max_stu];
+    double total[max_stu];
+    
+    for(int i=0;i<stu_num;i++){
+        char *p = new char[3];
+        double min_score = 1000, sum = 0;
+        cin>>p;
+        name[i] = p;
+
+        for(int j=0;j<ass_num;j++){
+            double demo;
+            cin>>demo;
+            demo = (demo<5*(ass_num-1)+50)?demo:5*(ass_num-1)+50;
+            min_score = min_score<demo?min_score:demo;
+            sum+=demo;
+        } 
+        
+        double exam;
+        cin>>exam;
+        sum += exam;
+        sum -= min_score;
+        total[i] = sum;
+    
+    }
+    
+    //adjust scores
+    double max_score = *max_element(total,total+stu_num);
+    for(int i=0;i<stu_num;i++){
+        int adjusted = ceil(total[i]*100/max_score);
+        cout<<name[i]<<' '<<total[i]<<' '<<adjusted<<' '<<letter(adjusted)<<endl;
+    }
+    
+    
+    
+    return 0;
 } 
